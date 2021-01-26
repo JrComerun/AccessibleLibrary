1db07ba4266008a3f2d1b8913ee52b8701143d43
JrComerun <kami621@mail.ru>
2021-01-26 12:55:11 -0800

    Header + nav with responsive is ready

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,11 @@ $(document).ready(function(){
   $(window).scroll(function(){
     if($(window).scrollTop()>60){
       nav.classList.add("nav-scrool");
+      $(".header-content").css("padding-top","150px")
     }
     else{
-    nav.classList.remove("nav-scrool");
+      nav.classList.remove("nav-scrool");
+      $(".header-content").css("padding-top","110px")
     }
   });
 })
