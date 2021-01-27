155d1c45c847bd2a7095940defa7da2b5f4fb95d
JrComerun <kami621@mail.ru>
2021-01-27 06:26:55 -0800

    Login Modal completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
-let nav = document.querySelector("nav");
-let navButton = document.querySelector(".button-nav");
-let selected= $(".selected")
 
-$(document).ready(function(){
+$(document).ready(function () {
+  // ***************************************************
+  //                Navigation On Scrool
+  //****************************************************
+  let nav = document.querySelector("nav");
+  let navButton = document.querySelector(".button-nav");
   $(window).scroll(function(){
     if($(window).scrollTop()>60){
       nav.classList.add("nav-scrool");
@@ -13,4 +15,58 @@ $(document).ready(function(){
       $(".header-content").css("padding-top","110px")
     }
   });
+
+
+  // ***************************************************
+  //                Selected On Scrool
+  //****************************************************
+  let selected= $(".selected")
+  $(window).scroll(function(){
+    if($(window).scrollTop()>400 ){
+    $("#Selected").css("transform","translateX(0)")
+    $("#Selected").css("transition","0.5s")
+    }
+    else{
+    $("#Selected").css("transform","translateX(100px)")
+    $("#Selected").css("transition","0.5s")
+    $(".selected-box").removeClass("show")
+    $(".selected-box i").css("fontSize","0px")
+    $(".selected-box a").css("width","0px")
+    }
+  });
+
+  
+  // ***************************************************
+  //                Selected On Scrool
+  //****************************************************
+  selected.click(function () {
+  if(!$(".selected-box").hasClass("show"))
+   {
+    $(".selected-box").addClass("show")
+    $(".selected-box i").css("fontSize","60px")
+    $(".selected-box a").css("width","80px")
+   }
+   else
+   {
+    $(".selected-box").removeClass("show")
+    $(".selected-box i").css("fontSize","0px")
+    $(".selected-box a").css("width","0px")
+   }
+  })
+
+
+
+
+
+
+
+
+
+
+
 })
+
+
+
+
+  
