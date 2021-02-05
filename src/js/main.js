3bc03b6af40c582f22fc83279bd903f6f803f138
ElgunMaqsudzade <elgunvm@code.edu.az>
2021-02-06 03:58:30 +0400

    Login Register modals were modified

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
-
 // ***************************************************
 //             PreLoader By JrC
 //****************************************************
-window.addEventListener('load', (event) => {
+window.addEventListener("load", (event) => {
   $(".overlay").fadeOut();
 });
 
@@ -14,19 +13,21 @@ $(document).ready(function () {
   $(window).scroll(function () {
     if ($(window).scrollTop() > 60) {
       nav.classList.add("nav-scroll");
-      $(".header-bar").css("padding-bottom", "40px")
-    }
-    else {
+      $(".header-bar").css("padding-bottom", "40px");
+    } else {
       nav.classList.remove("nav-scroll");
-      $(".header-bar").css("padding-bottom", "0px")
+      $(".header-bar").css("padding-bottom", "0px");
     }
   });
-
-  
-})
-
-
-
-
-
-  
+  // ***************************************************
+  //                Modal login & register switch
+  //****************************************************
+});
+function SwitchToReg() {
+  $(".login-part-modal").css({ transform: "translateX(-1000px)", opacity: "0" });
+  $(".register-part-modal").css({ transform: "translateX(-50%)", opacity: "1" });
+}
+function SwitchToLog() {
+  $(".register-part-modal").css({ transform: "translateX(800px)", opacity: "0" });
+  $(".login-part-modal").css({ transform: "translateX(-50%)", opacity: "1" });
+}
