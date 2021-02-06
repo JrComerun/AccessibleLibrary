54b14784afd9a518b659abdce7c0e00099722fcf
JrComerun <kami621@mail.ru>
2021-02-05 16:08:00 -0800

    old version

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
+
 // ***************************************************
 //             PreLoader By JrC
 //****************************************************
-window.addEventListener("load", (event) => {
+window.addEventListener('load', (event) => {
   $(".overlay").fadeOut();
 });
 
@@ -13,26 +14,13 @@ $(document).ready(function () {
   $(window).scroll(function () {
     if ($(window).scrollTop() > 60) {
       nav.classList.add("nav-scroll");
-      $(".header-bar").css("padding-bottom", "40px");
-    } else {
+      $(".header-bar").css("padding-bottom", "40px")
+    }
+    else {
       nav.classList.remove("nav-scroll");
-      $(".header-bar").css("padding-bottom", "0px");
+      $(".header-bar").css("padding-bottom", "0px")
     }
   });
-<<<<<<< HEAD
-  // ***************************************************
-  //                Modal login & register switch
-  //****************************************************
-});
-function SwitchToReg() {
-  $(".login-part-modal").css({ transform: "translateX(-1000px)", opacity: "0" });
-  $(".register-part-modal").css({ transform: "translateX(-50%)", opacity: "1" });
-}
-function SwitchToLog() {
-  $(".register-part-modal").css({ transform: "translateX(800px)", opacity: "0" });
-  $(".login-part-modal").css({ transform: "translateX(-50%)", opacity: "1" });
-}
-=======
   
   
 })
@@ -42,4 +30,3 @@ function SwitchToLog() {
 
 
   
->>>>>>> 63960b774f163293856c74b0d5e307e0a0b244b7
